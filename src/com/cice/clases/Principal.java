3e9281f7a065b4cd0e43ccb98a679f8b2173bbe9
cice <cice@192.168.3.67>
2017-06-21 21:03:18 +0200

    añadida lineas de codigo

---
+++ src/com/cice/clases/Principal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cice.clases;
+
+/**
+ *
+ * @author cice
+ */
+public class Principal {
+
+    public static void main (String [] args){
+    
+       //ahora vamos a crear los coches
+       
+       Coche coche1 = new Coche();
+       
+        
+        
+        
+            }
+
+    
+}
