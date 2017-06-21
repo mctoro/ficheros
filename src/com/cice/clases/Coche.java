c6de9e8cb386701ed36c2b19fec12793b76fb4eb
cice <cice@192.168.3.67>
2017-06-21 20:34:49 +0200

    añadida lineas de codigo

---
+++ src/com/cice/clases/Coche.java
@@ -0,0 +1,42 @@
+
+package com.cice.clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author cice
+ */
+public class Coche {
+    
+    //atributos o variables
+    //caracteristicas
+    String modelo;
+    String color;
+    String matricula;
+    String bastidor;
+    int potencia;
+    Date fecha;
+    boolean isRiding;
+    boolean isOn;
+    
+    
+    //funciones
+    //ambitos "publico" "por defecto" "privado"
+    void acelerar(){
+        
+    }
+    
+    void frenar(){
+    
+    }
+    
+    void arrancar(){
+    
+    }
+    
+    void apagar(){
+    
+    }
+    
+}
